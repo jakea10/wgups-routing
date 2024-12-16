cedf24ea9ee5b4a40ee5b6d76a6ca677292339d4
jakea10 <jakeatencio1@gmail.com>
2024-12-15 20:19:51 -0700

    Add custom hash_table implementation and associated tests. This implementation uses linear probing to handle collisions and eager resizing based on a 0.6 load factor

---
+++ src/hash_table.py
@@ -0,0 +1,183 @@
+# hash_table.py
+
+from typing import NamedTuple, Any
+
+
+DELETED = object()  # sentinel value for linear probing
+
+
+class Pair(NamedTuple):
+    key: Any
+    value: Any
+
+
+class HashTable:
+    def __init__(
+            self,
+            capacity: int = 8,
+            load_factor_threshold: float = 0.6
+        ):
+        if isinstance(capacity, bool) or not isinstance(capacity, int) or capacity < 1:
+            raise ValueError("Capacity must be a positive integer")
+        if not (0 < load_factor_threshold <= 1):
+            raise ValueError("Load factor must be a number between (0 and 1]")
+        self._slots = capacity * [None]
+        self._load_factor_threshold = load_factor_threshold
+
+
+    def __len__(self):
+        return len(self.pairs)
+    
+
+    def __setitem__(self, key, value):
+        if self.load_factor >= self._load_factor_threshold:
+            self._resize_and_rehash()
+
+        for index, pair in self._probe(key):
+            if pair is DELETED: continue  # collsion has occurred before
+            if pair is None or pair.key == key:  # new key or update
+                self._slots[index] = Pair(key, value)
+                break
+
+
+    def __getitem__(self, key):
+        for _, pair in self._probe(key):
+            if pair is None:
+                raise KeyError(key)
+            if pair is DELETED:
+                continue
+            if pair.key == key:
+                return pair.value
+        raise KeyError(key)
+    
+
+    def __delitem__(self, key):
+        for index, pair in self._probe(key):
+            if pair is None:
+                raise KeyError(key)
+            if pair is DELETED:
+                continue
+            if pair.key == key:
+                self._slots[index] = DELETED
+                return
+        raise KeyError(key)
+    
+
+    def __contains__(self, key):
+        try:
+            self[key]
+        except KeyError:
+            return False
+        else:
+            return True
+        
+    
+    def __iter__(self):
+        yield from self.keys
+    
+
+    def __str__(self):
+        pairs = []
+        for key, value in self.pairs:
+            pairs.append(f"{key!r}: {value!r}")
+        return "{" + ", ".join(pairs) + "}"
+    
+
+    def __repr__(self):
+        cls = self.__class__.__name__
+        return f"{cls}.from_dict({str(self)})"
+    
+
+    def __eq__(self, other):
+        if self is other:  # same id()
+            return True
+        if type(self) is not type(other):
+            return False
+        return set(self.pairs) == set(other.pairs)
+    
+
+    def _index(self, key) -> int:
+        return hash(key) % self.capacity
+
+
+    def _probe(self, key):
+        index = self._index(key)
+        for _ in range(self.capacity):
+            yield index, self._slots[index]
+            index = (index + 1) % self.capacity  # modulo wraps index around origin if necessary
+
+    def _resize_and_rehash(self):
+        copy = HashTable(capacity=self.capacity * 2)
+        for key, value in self.pairs:
+            copy[key] = value
+        self._slots = copy._slots
+
+
+    def get(self, key, default = None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+    
+
+    def copy(self):
+        return HashTable.from_dict(dict(self.pairs), self.capacity)
+    
+
+    def clear(self):
+        self._slots = self.capacity * [None]
+    
+
+    @property
+    def pairs(self):
+        return {
+            pair for pair in self._slots
+            if pair not in (None, DELETED)
+        }
+    
+
+    @property
+    def values(self):
+        return [pair.value for pair in self.pairs]
+    
+
+    @property
+    def keys(self):
+        return {pair.key for pair in self.pairs}
+    
+
+    @property
+    def capacity(self):
+        return len(self._slots)
+    
+
+    @property
+    def load_factor(self):
+        occupied_or_deleted = [slot for slot in self._slots if slot]
+        return len(occupied_or_deleted) / self.capacity
+
+
+    @classmethod
+    def from_dict(cls, dictionary: dict, capacity: int = None):
+        hash_table = cls(capacity or len(dictionary) * 2)
+        for key, value in dictionary.items():
+            hash_table[key] = value
+        return hash_table
+    
+
+if __name__ == "__main__":
+    # ----------------------------------------------------------
+    # ad hoc testing
+    # ----------------------------------------------------------
+
+    # Visualize dynamic resizing
+    ht = HashTable()
+
+    for i in range(50):
+        num_pairs = len(ht)
+        num_empty = ht.capacity - num_pairs
+        print(
+            f"{num_pairs:>2}/{ht.capacity:>2}",
+            ("▣ " * num_pairs) + ("□ " * num_empty)
+        )
+        ht[i] = i
---
+++ tests/test_hash_table.py
@@ -0,0 +1,331 @@
+from src.hash_table import HashTable, Pair, DELETED
+import pytest
+from pytest_unordered import unordered
+from unittest.mock import patch
+
+
+# def test_should_always_pass():
+#     assert 2 + 2 == 4, "This is just a dummy test"
+
+
+def test_should_create_hash_table():
+    assert HashTable(capacity=100) is not None
+
+
+def test_should_not_create_hash_table_with_zero_capacity():
+    with pytest.raises(ValueError):
+        HashTable(capacity=0)
+
+
+def test_should_not_create_hash_table_with_negative_capacity():
+    with pytest.raises(ValueError):
+        HashTable(capacity=-100)
+
+
+def test_should_not_create_hash_table_with_non_int_capacity():
+    args = ["one", 2.5, True]
+    for arg in args:
+        with pytest.raises(ValueError):
+            HashTable(capacity=arg)
+
+
+def test_should_report_capacity_of_empty_hash_table():
+    assert HashTable(capacity=100).capacity == 100
+
+
+def test_should_report_capacity(hash_table):
+    assert hash_table.capacity == 100
+
+
+def test_should_report_length_of_empty_hash_table():
+    hash_table = HashTable(capacity=100)
+    assert len(hash_table) == 0
+
+
+def test_should_report_length(hash_table):
+    assert len(hash_table) == 4
+
+
+def test_should_create_empty_value_slots():
+    assert HashTable(capacity=3)._slots == [None, None, None]
+
+
+def test_should_not_contain_none_value_when_created():
+    assert None not in HashTable(capacity=10).values
+
+
+def test_should_insert_key_value_pairs():
+    hash_table = HashTable(capacity=100)
+    hash_table["hello"] = "world"
+    hash_table[98.6] = 37
+    hash_table[True] = False
+    hash_table["key"] = None
+    assert len(hash_table) == 4
+
+
+@pytest.fixture
+def hash_table() -> HashTable:
+    sample_data = HashTable(capacity=100)
+    sample_data["hello"] = "world"
+    sample_data[98.6] = 37
+    sample_data[True] = False
+    sample_data["key"] = None
+    return sample_data
+
+
+def test_should_find_value_by_key(hash_table):
+    assert hash_table["hello"] == "world"
+    assert hash_table[98.6] == 37
+    assert hash_table[True] is False
+    assert hash_table["key"] is None
+
+
+def test_should_raise_error_on_missing_key():
+    hash_table = HashTable(capacity=10)
+    with pytest.raises(KeyError) as exception_info:
+        hash_table["missing_key"]
+    assert exception_info.value.args[0] == "missing_key"
+
+
+def test_should_find_key(hash_table):
+    assert "hello" in hash_table
+
+
+def test_should_not_find_key(hash_table):
+    assert "missing_key" not in hash_table
+
+
+def test_should_get_value(hash_table):
+    assert hash_table.get("hello") == "world"
+
+
+def test_should_get_none_when_missing_key(hash_table):
+    assert hash_table.get("missing_key") is None
+
+
+def test_should_get_default_value_when_missing_key(hash_table):
+    assert hash_table.get("missing_key", "default") == "default"
+
+
+def test_should_get_value_with_default(hash_table):
+    assert hash_table.get("hello", "default") == "world"
+
+
+def test_should_delete_key_value_pair(hash_table):
+    assert "hello" in hash_table
+    assert ("hello", "world") in hash_table.pairs
+    assert len(hash_table) == 4
+
+    del hash_table["hello"]
+
+    assert "hello" not in hash_table
+    assert ("hello", "world") not in hash_table.pairs
+    assert len(hash_table) == 3
+
+
+def test_should_raise_key_error_when_deleting(hash_table):
+    with pytest.raises(KeyError) as exception_info:
+        del hash_table["missing_key"]
+    assert exception_info.value.args[0] == "missing_key"
+
+
+def test_should_update_value(hash_table):
+    assert hash_table["hello"] == "world"
+
+    hash_table["hello"] = "there"
+
+    assert hash_table["hello"] == "there"
+    assert hash_table[98.6] == 37
+    assert hash_table[True] is False
+    assert hash_table["key"] is None
+    assert len(hash_table) == 4
+
+
+def test_should_return_pairs(hash_table):
+    assert hash_table.pairs == {
+        ("hello", "world"),
+        (98.6, 37),
+        (True, False),
+        ("key", None)
+    }
+
+
+def test_should_get_pairs_of_empty_hash_table():
+    assert HashTable(capacity=10).pairs == set()
+
+
+def test_should_return_copy_of_pairs(hash_table):
+    # Test defensive copying
+    assert hash_table.pairs is not hash_table.pairs
+
+
+def test_should_not_include_blank_pairs(hash_table):
+    assert None not in hash_table.pairs
+
+
+def test_should_return_duplicate_values():
+    hash_table = HashTable(capacity=100)
+    hash_table["Harry"] = "Gryffindor"
+    hash_table["Draco"] = "Slytherin"
+    hash_table["Neville"] = "Gryffindor"
+    assert hash_table.values.count("Gryffindor") == 2
+
+
+def test_should_get_values(hash_table):
+    # Don't take order into account when comparing
+    assert unordered(hash_table.values) == ["world", 37, False, None]
+
+
+def test_should_get_values_of_empty_hash_table():
+    assert HashTable(capacity=100).values == []
+
+
+def test_should_return_copy_of_values(hash_table):
+    assert hash_table.values is not hash_table.values
+
+
+def test_should_get_keys(hash_table):
+    assert hash_table.keys == {"hello", 98.6, True, "key"}
+
+
+def test_should_get_keys_of_empty_hash_table():
+    assert HashTable(capacity=10).keys == set()  # empty set
+
+
+def test_should_return_copy_of_keys(hash_table):
+    assert hash_table.keys is not hash_table.keys
+
+
+def test_should_convert_to_dict(hash_table):
+    dictionary = dict(hash_table.pairs)
+    assert set(dictionary.keys()) == hash_table.keys
+    assert set(dictionary.items()) == hash_table.pairs
+    assert list(dictionary.values()) == unordered(hash_table.values)
+
+
+def test_should_iterate_over_keys(hash_table):
+    for key in hash_table.keys:
+        assert key in ("hello", 98.6, True, "key")
+
+
+def test_should_iterate_over_values(hash_table):
+    for key in hash_table.values:
+        assert key in ("world", 37, False, None)
+
+
+def test_should_iterate_over_pairs(hash_table):
+    for key, value in hash_table.pairs:
+        assert key in hash_table.keys
+        assert value in hash_table.values
+
+
+def test_should_iterate_over_instance(hash_table):
+    for key in hash_table:
+        assert key in ("hello", 98.6, True, "key")
+
+
+def test_should_use_dict_literal_for_str(hash_table):
+    del hash_table["key"]  # for less permutations to check
+    assert str(hash_table) in {
+        "{'hello': 'world', 98.6: 37, True: False}",
+        "{'hello': 'world', True: False, 98.6: 37}",
+        "{98.6: 37, 'hello': 'world', True: False}",
+        "{98.6: 37, True: False, 'hello': 'world'}",
+        "{True: False, 'hello': 'world', 98.6: 37}",
+        "{True: False, 98.6: 37, 'hello': 'world'}",
+    }
+
+
+def test_should_create_hash_table_from_dict():
+    dictionary = {"hello": "world", 98.6: 37, True: False}
+    hash_table: HashTable = HashTable.from_dict(dictionary)
+
+    assert hash_table.capacity == len(dictionary) * 2
+    assert hash_table.keys == set(dictionary.keys())
+    assert hash_table.pairs == set(dictionary.items())
+    assert unordered(hash_table.values) == list(dictionary.values())
+
+
+def test_should_create_hash_table_from_dict_with_custom_capacity():
+    dictionary = {"hello": "world", 98.6: 37, True: False}
+    hash_table: HashTable = HashTable.from_dict(dictionary, 50)
+
+    assert hash_table.capacity == 50
+
+
+def test_should_have_canonical_string_representation(hash_table):
+    del hash_table["key"]  # for less permutations to check
+    assert repr(hash_table) in {
+        "HashTable.from_dict({'hello': 'world', 98.6: 37, True: False})",
+        "HashTable.from_dict({'hello': 'world', True: False, 98.6: 37})",
+        "HashTable.from_dict({98.6: 37, 'hello': 'world', True: False})",
+        "HashTable.from_dict({98.6: 37, True: False, 'hello': 'world'})",
+        "HashTable.from_dict({True: False, 'hello': 'world', 98.6: 37})",
+        "HashTable.from_dict({True: False, 98.6: 37, 'hello': 'world'})",
+    }
+
+
+def test_should_compare_equal_to_itself(hash_table):
+    assert hash_table == hash_table
+
+
+def test_should_compare_equal_to_copy(hash_table):
+    assert hash_table is not hash_table.copy()
+    assert hash_table == hash_table.copy()
+
+
+def test_should_compare_equal_different_key_value_order():
+    h1 = HashTable.from_dict({'a': 1, 'b': 2, 'c': 3})
+    h2 = HashTable.from_dict({'b': 2, 'c': 3, 'a': 1})
+    assert h1 == h2
+
+
+def test_should_compare_unequal(hash_table):
+    other = HashTable.from_dict({'different': 'value'})
+    assert hash_table != other
+
+
+def test_should_compare_unequal_another_data_type(hash_table):
+    assert hash_table != 42
+
+
+def test_should_clear_key_value_pairs(hash_table):
+    assert len(hash_table) == 4
+    hash_table.clear()
+    assert len(hash_table) == 0
+
+
+@pytest.fixture
+def collision_ht() -> HashTable:
+    with patch('builtins.hash', return_value=24):
+        ht = HashTable(capacity=100)
+        ht['easy'] = 'Requires little effort'
+        ht['medium'] = 'Requires some skill and effort'
+        ht['difficult'] = 'Needs much skill'
+    return ht
+
+
+def test_should_handle_collisions_on_create(collision_ht: HashTable):
+    # linear probe
+    assert collision_ht._slots[24] == Pair('easy', 'Requires little effort')
+    assert collision_ht._slots[25] == Pair('medium', 'Requires some skill and effort')
+    assert collision_ht._slots[26] == Pair('difficult', 'Needs much skill')
+
+
+def test_should_handle_collisions_on_read(collision_ht: HashTable):
+    with patch('builtins.hash', return_value=24):
+        assert collision_ht['easy'] == 'Requires little effort'
+        assert collision_ht['medium'] == 'Requires some skill and effort'
+        assert collision_ht['difficult'] == 'Needs much skill'
+
+
+def test_should_handle_collisions_on_update(collision_ht: HashTable):
+    with patch('builtins.hash', return_value=24):
+        collision_ht['difficult'] = 'Requires much skill'
+        assert collision_ht['difficult'] == 'Requires much skill'
+
+
+def test_should_handle_collisions_on_delete(collision_ht: HashTable):
+    with patch('builtins.hash', return_value=24):
+        del collision_ht['medium']
+        assert collision_ht._slots[25] is DELETED
