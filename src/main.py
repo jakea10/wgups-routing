62b6c44f4751e4f73f60bd3b8eb07ebca391fab1
jakea10 <jakeatencio1@gmail.com>
2025-05-26 11:59:25 -0600

    refactor distances.csv; add functions for parsing address and distance data

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import datetime
 
 EOD_TIME = datetime.time(17, 0)  # 5:00 PM
 
+
 def load_package_data(package_file: str, hash_table: HashTable) -> None:
     """
     Parses package data from a CSV file into the provided hash table.
@@ -39,8 +40,50 @@ def load_package_data(package_file: str, hash_table: HashTable) -> None:
             hash_table[package.id] = package
 
 
-def load_address_data(address_file: str):
-    pass
+def load_address_data(address_file: str) -> tuple[dict, list]:
+    """
+    Parses address data from a CSV file.
+    """
+    address_to_id_map = {}  # Maps addresses to corresponding indices in distance matrix
+    id_to_address_map = []  # For reverse lookup (i.e. lookup address by ID)
+
+    with open(address_file, mode='r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+
+        for row in reader:
+            id = int(row['id'])
+            address = row['address']
+
+            address_to_id_map[address] = id
+            id_to_address_map.insert(id, address)
+
+    return (address_to_id_map, id_to_address_map)
+
+
+def load_distance_data(distance_file: str, num_addresses: int, header_row: bool = False) -> list[list]:
+    """
+    Parses distance data from a CSV file.
+    """
+    # Initialize a 2D matrix based on num_addresses with zeros as a placeholder
+    distance_matrix = [[0.0 for _ in range(num_addresses)] for _ in range(num_addresses)]
+
+    with open(distance_file, mode='r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        if header_row:
+            reader.__next__()  # Skip header row
+
+        current_row_index = 0
+        for row in reader:
+            for col_index in range(len(row)):
+                distance_value = float(row[col_index])
+
+                # The distance matrix is symmetric, i.e. distance(A,B) == distance(B,A)
+                distance_matrix[current_row_index][col_index] = distance_value
+                distance_matrix[col_index][current_row_index] = distance_value
+
+            current_row_index += 1
+
+    return distance_matrix
 
 
 # ------------------------------------------------------------------------------
@@ -55,13 +98,34 @@ packages = HashTable(capacity=100)
 # ------------------------------------------------------------------------------
 package_file = "wgups-routing/data/packages.csv"
 load_package_data(package_file, packages)
-print(packages[2])
+# print(packages[2])
+
+# ------------------------------------------------------------------------------
+# Load address data
+# ------------------------------------------------------------------------------
+address_file = "wgups-routing/data/addresses.csv"
+address_to_id_map, id_to_address_map = load_address_data(address_file)
+num_addresses = len(id_to_address_map)
+# print(address_to_id_map)
+# print()
+# print(id_to_address_map)
 
 # ------------------------------------------------------------------------------
 # Load distance data
 # ------------------------------------------------------------------------------
+distance_file = "wgups-routing/data/distances_new.csv"
+distance_matrix = load_distance_data(distance_file, num_addresses)
+# for i in range(len(distance_matrix)):
+#     print(f"{id_to_address_map[i]}: {distance_matrix[i]}")
 
+package_1: Package = packages.get(1)
+package_2: Package = packages.get(2)
 
-# ------------------------------------------------------------------------------
-# 
-# ------------------------------------------------------------------------------
+address_1_id = address_to_id_map.get(package_1.address)
+address_2_id = address_to_id_map.get(package_2.address)
+
+if address_1_id is not None and address_2_id is not None:
+    distance = distance_matrix[address_1_id][address_2_id]
+    print("Distance between package 1 and package 2 addresses:", distance)
+else:
+    print("Could not find address ID for one or both packages.")
